0c06e93a25375cc4b010e6ed1930d232651cb41b
Egbudom-Raphael <egbudomraphael@gmail.com>
2023-03-15 01:21:28 +0000

    initial commit

---
+++ dnn_gui.py
@@ -0,0 +1,152 @@
+import streamlit as st  # web development
+import numpy as np  # np mean, np random
+import pandas as pd  # read csv, df manipulation
+import time  # to simulate a real time data, time loop
+from tensorflow import keras
+from sklearn.preprocessing import StandardScaler
+import plotly.express as px  # interactive charts
+from millify import millify
+import requests
+import json
+
+st.set_page_config(
+    page_title='Time Series Dashboard',
+    page_icon='⏳',
+    layout='wide'
+)
+
+# dashboard title
+st.title("Timeseries Anomaly Detection")
+st.write('''
+Real-time anomaly detection for **timeseries** in terms of credit card fraud transaction detection.
+#### ***Description***
+The dataset contains transactions made by credit cards in ***September 2013*** by European cardholders.
+''')
+st.markdown("""---""")
+
+
+def data_collector():
+    # read csv from a github repo
+    data = pd.read_csv("creditcard.csv")
+    data = data.drop_duplicates()
+    return data
+
+
+def plot_time_graph(data):
+    plot = px.bar(data, x='Time (second)', y='Amount',
+                   color='Class', color_discrete_sequence=['green', "red"])
+    plot.update_layout(width=1300, height=500)
+    return plot
+def plot_amount_graph(data):
+    plot = px.bar(data, x='Class', y='Amount',
+                   color='Class', color_discrete_sequence=['green', "red"])
+    return plot
+
+def data_preprocessing(row):
+    sc = StandardScaler()
+    chosen_features = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13',
+                       'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27',
+                       'V28']
+    data=data_collector()
+    X = data[chosen_features]
+    X = sc.fit_transform(X)
+    X = pd.DataFrame(X, columns=chosen_features)
+    first_row = X.iloc[row]
+    data = data.drop('Class', axis=1)
+    original_df = data.iloc[row]
+    # convert the first row to a dictionary with column names as keys
+    row_dict = first_row.to_dict()
+    return [row_dict, original_df]
+
+def predict_model(row):
+    url = 'http://127.0.0.1:8000/predict'
+    data = data_preprocessing(row)
+    input_json = json.dumps(data[0])
+    response = requests.post(url, data=input_json)
+    pred = round(eval(eval(response.text)))
+    pred = pd.DataFrame([pred], columns=['Class'])
+    X=pd.DataFrame(data[1].to_frame().T)
+    X.reset_index(drop=True, inplace=True)
+    result = X.join(pred)
+    return result
+
+# creating a single-element container.
+frame = st.empty()
+def count_function(num):
+    # X = data_collector()
+    subset = pd.DataFrame()
+    subset2 = pd.DataFrame()
+    fraud_df = pd.DataFrame(columns=['Time (second)','Amount'])
+    fraud = 0
+    valid = 0
+    for x in range(num):
+        X = predict_model(x)
+        for i, row in X.iterrows():
+            subset = pd.concat([subset, row.to_frame().T], ignore_index=True)
+            subset2 = pd.concat([subset2, row.to_frame().T], ignore_index=True)
+            avg=subset["Amount"].mean()
+            if subset.loc[0, 'Class'] == 1:
+                fraud_df = pd.concat([fraud_df, subset.loc[0].to_frame().T], ignore_index=True)
+                fraud_df.loc[fraud, 'Time (second)'] = pd.to_datetime(subset.loc[0, 'Time (second)'], unit='s')
+                fraud_df = fraud_df.drop(['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13',
+                                          'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24',
+                                          'V25', 'V26', 'V27', 'V28', 'Class'], axis=1)
+                fraud += 1
+            elif subset.loc[0, 'Class'] == 0:
+                valid += 1
+            if len(subset) < 17:
+                pass
+            else:
+                subset.drop(index=subset.index[0], axis=0, inplace=True)
+
+            with frame.container():
+                # create three columns
+                avg_amt, valid_count, fraud_count = st.columns(3)
+                # fill in those three columns with respective metrics
+                avg_amt.metric(label="Average Amount 💲",
+                               value=f"$ {round(avg)}",)
+                valid_count.metric(
+                    label="Number of Legitimate transactions ✅", value=f'{millify(valid)}',delta=1)
+                fraud_count.metric(
+                    label="Number of Fraudulent transctions ❌", value=f'{millify(fraud)}', delta=1, delta_color="inverse")
+                st.markdown("""---""")
+                fig_col1, fig_col2 = st.columns(2)
+                with fig_col1:
+                    st.markdown("### Transactions over time")
+                    st.write(plot_time_graph(subset))
+                # with fig_col2:
+                st.markdown("""---""")
+                tab_col1, tab_col2 = st.columns(2)
+                with tab_col1:
+                    st.markdown('### Fraudulent transactions')
+                    st.write(fraud_df)
+                with tab_col2:
+                    st.markdown('### Sum Total Amounts')
+                    st.write(plot_amount_graph(subset2))
+        # time.sleep(0.01)
+
+# count_function()
+
+
+def begin():
+    count_function(10000)
+    # pass
+
+
+
+button1=st.sidebar.button('Launch', type='primary', use_container_width=True)
+button2=st.sidebar.button('Interrupt', type='secondary', use_container_width=True)
+st.sidebar.markdown("""
+# GROUP 25
+
+ Raphael Egbudom K22723032
+
+ Ehiaghe Oziegbe K22570843
+
+ Amiede Iyamu K2225150
+""")
+if button1:
+    begin()
+
+if button2:
+    st.stop()
---
+++ fast_api.py
@@ -0,0 +1,37 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tensorflow import keras
+from typing import Dict
+# import json
+import pandas as pd
+
+app=FastAPI()
+
+origins=['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.post("/predict")
+def predict(data: Dict):
+    # Convert the input data to a pandas DataFrame
+    df = pd.DataFrame.from_dict(data, orient='index').T
+
+    # loading the saved model
+    model = keras.models.load_model('1d_conv.hdf5', compile=False)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Make a prediction using the trained model
+    y_pred = model.predict(df)
+    print(y_pred)
+    # Return the prediction as a dictionary
+    return f'{y_pred[0][0]}'
+
+if __name__=='__main__':
+    uvicorn.run(app)
