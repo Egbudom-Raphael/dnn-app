60b7f7e24ee209f3f49448e8a0fffda8bb7bb4ad
Egbudom-Raphael <egbudomraphael@gmail.com>
2023-03-15 23:50:02 +0000

    added user data input

--- dnn_gui.py
+++ dnn_gui.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import plotly.express as px  # interactive charts
 from millify import millify
 import requests
+# import time
 import json
 
 st.set_page_config(
@@ -20,18 +21,19 @@ Real-time anomaly detection for **timeseries** in terms of credit card fraud tra
 The dataset contains transactions made by credit cards in ***September 2013*** by European cardholders.
 ''')
 st.markdown("""---""")
-
-
-def data_collector():
-    # read csv from a github repo
-    data = pd.read_csv("creditcard.csv")
-    data = data.drop_duplicates()
-    return data
-
+file = st.sidebar.file_uploader('## Upload your data', type='csv')
+if file is not None:
+    data_store = pd.read_csv(file)
+    data_store = data_store.drop_duplicates()
+else:
+    st.warning('Warning, Upload Csv File format to begin...')
 
 def plot_time_graph(data):
-    plot = px.bar(data, x='Time (second)', y='Amount',
-                   color='Class', color_discrete_sequence=['green', "red"])
+    plot = px.bar(data, x='Time (second)', y='Amount',color='Class', color_discrete_sequence=['green', "red"])
+    plot.update_layout(width=1300, height=500)
+    return plot
+def plot_scatter_plot(data):
+    plot = px.scatter(data, x='Time (second)', y='Amount',size='Amount',symbol='Class')
     plot.update_layout(width=1300, height=500)
     return plot
 def plot_amount_graph(data):
@@ -44,7 +46,7 @@ def data_preprocessing(row):
     chosen_features = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13',
                        'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27',
                        'V28']
-    data=data_collector()
+    data=data_store
     X = data[chosen_features]
     X = sc.fit_transform(X)
     X = pd.DataFrame(X, columns=chosen_features)
@@ -56,7 +58,8 @@ def data_preprocessing(row):
     return [row_dict, original_df]
 
 def predict_model(row):
-    url = 'https://dnn-api.onrender.com/predict'
+    # url = 'https://dnn-api.onrender.com/predict'
+    url='http://127.0.0.1:8000/predict'
     data = data_preprocessing(row)
     input_json = json.dumps(data[0])
     response = requests.post(url, data=input_json)
@@ -70,7 +73,6 @@ def predict_model(row):
 # creating a single-element container.
 frame = st.empty()
 def count_function(num):
-    # X = data_collector()
     subset = pd.DataFrame()
     subset2 = pd.DataFrame()
     fraud_df = pd.DataFrame(columns=['Time (second)','Amount'])
@@ -83,15 +85,15 @@ def count_function(num):
             subset2 = pd.concat([subset2, row.to_frame().T], ignore_index=True)
             avg=subset["Amount"].mean()
             if subset.loc[0, 'Class'] == 1:
+                fraud += 1
                 fraud_df = pd.concat([fraud_df, subset.loc[0].to_frame().T], ignore_index=True)
                 fraud_df.loc[fraud, 'Time (second)'] = pd.to_datetime(subset.loc[0, 'Time (second)'], unit='s')
                 fraud_df = fraud_df.drop(['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13',
                                           'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24',
                                           'V25', 'V26', 'V27', 'V28', 'Class'], axis=1)
-                fraud += 1
             elif subset.loc[0, 'Class'] == 0:
                 valid += 1
-            if len(subset) < 17:
+            if len(subset) < 15:
                 pass
             else:
                 subset.drop(index=subset.index[0], axis=0, inplace=True)
@@ -113,6 +115,7 @@ def count_function(num):
                     st.write(plot_time_graph(subset))
                 # with fig_col2:
                 st.markdown("""---""")
+                st.write(plot_scatter_plot(subset))
                 tab_col1, tab_col2 = st.columns(2)
                 with tab_col1:
                     st.markdown('### Fraudulent transactions')
@@ -120,18 +123,21 @@ def count_function(num):
                 with tab_col2:
                     st.markdown('### Sum Total Amounts')
                     st.write(plot_amount_graph(subset2))
-        # time.sleep(0.01)
+        # time.sleep(0.9)
 
 # count_function()
 
 
 def begin():
-    count_function(10000)
+    count_function(len(data_store))
     # pass
 
 
+if file:
+    button1=st.sidebar.button('Launch', type='primary', use_container_width=True)
+else:
+    button1=st.sidebar.button('Launch',disabled=True, type='primary', use_container_width=True)
 
-button1=st.sidebar.button('Launch', type='primary', use_container_width=True)
 button2=st.sidebar.button('Interrupt', type='secondary', use_container_width=True)
 st.sidebar.markdown("""
 # GROUP 25
